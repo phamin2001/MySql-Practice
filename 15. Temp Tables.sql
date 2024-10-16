1f098f545e656b54803d8c898a902eac4fc12c48
phamin2001 <amin.pahlavani@gmail.com>
2024-10-16 16:33:54 -0600

    temp table

---
+++ 15. Temp Tables.sql
@@ -0,0 +1,35 @@
+-- Temporary Tables
+
+CREATE TEMPORARY TABLE temp_table
+(first_name varchar(50),
+last_name VARCHAR(50),
+favorite_movie VARCHAR(100)
+);
+
+SELECT *
+FROM temp_table;
+
+INSERT INTO temp_table
+VALUES('Alex', 'Freberg', 'Lord of the Rings: The Two Towers');
+
+SELECT *
+FROM temp_table;
+
+SELECT *
+FROM employee_salary
+WHERE salary > 50000
+;
+
+CREATE TEMPORARY TABLE salary_over_50k
+SELECT * 
+FROM employee_salary
+WHERE salary >= 50000
+;
+
+SELECT *
+FROM salary_over_50k
+;
+
+
+
+
