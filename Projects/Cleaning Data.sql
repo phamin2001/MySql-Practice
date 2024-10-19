3bf33588642538bd79902c96fe9c7293844112a9
phamin2001 <amin.pahlavani@gmail.com>
2024-10-19 10:13:14 -0600

    Remove Duplicate

--- Projects/Cleaning Data.sql
+++ Projects/Cleaning Data.sql
@@ -23,9 +23,97 @@ IGNORE 1 ROWS
 (company, location, industry, total_laid_off, percentage_laid_off, date, stage, country, @funds_raised)
 SET funds_raised_millions = IF(TRIM(@funds_raised) = '' OR TRIM(@funds_raised) = 'NULL' OR NOT TRIM(@funds_raised) REGEXP '^-?[0-9]+$', NULL, TRIM(@funds_raised));
 
+SELECT COUNT(*) AS total_rows 
+FROM layoffs;
+
+SELECT *
+FROM layoffs;
+
+-- 1. Remove Duplicates
+-- 2. Standardize the Data
+-- 3. Null Values or Blank Values
+-- 4. Remove Any Columns 
+
+
+-- 1. Remove Duplicates
+CREATE TABLE  layoffs_staging
+LIKE layoffs;
+
+SELECT *
+FROM layoffs_staging;
+
+INSERT layoffs_staging
 SELECT *
 FROM layoffs;
 
 SELECT COUNT(*) AS total_rows 
-FROM layoffs;
+FROM layoffs_staging;
+
+SELECT *, 
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, 
+total_laid_off, percentage_laid_off, `date`, stage, 
+country, funds_raised_millions) AS row_num
+FROM layoffs_staging;
+
+WITH duplicate_cte AS
+(SELECT *, 
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, 
+total_laid_off, percentage_laid_off, `date`, stage, 
+country, funds_raised_millions) AS row_num
+FROM layoffs_staging
+)
+SELECT *
+FROM duplicate_cte 
+WHERE row_num > 1;
+
+SELECT *
+FROM layoffs_staging
+WHERE company = 'Casper';
+
+
+
+CREATE TABLE `layoffs_staging2`(
+    `company` TEXT,
+    `location` TEXT,
+    `industry` TEXT,
+    `total_laid_off` INT DEFAULT NULL,
+    `percentage_laid_off` TEXT,
+    `date` TEXT,
+    `stage` TEXT,
+    `country` TEXT,
+    `funds_raised_millions` INT DEFAULT NULL,
+    `row_num` INT
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+SELECT *
+FROM layoffs_staging2;
+
+INSERT INTO layoffs_staging2
+SELECT *, 
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, 
+total_laid_off, percentage_laid_off, `date`, stage, 
+country, funds_raised_millions) AS row_num
+FROM layoffs_staging;
+
+SELECT *
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+DELETE 
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+SELECT *
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+SELECT *
+FROM layoffs_staging2;
+
+SELECT COUNT(*) AS total_rows 
+FROM layoffs_staging2;
+
 
