3a8ebcc5d8a4ebf11d779ce7bcb07867581652dc
phamin2001 <amin.pahlavani@gmail.com>
2024-10-23 20:32:12 -0600

    exploratory data

--- Projects/Cleaning Data.sql
+++ Projects/Cleaning Data.sql
@@ -226,6 +226,18 @@ FROM layoffs_staging2
 WHERE total_laid_off IS NULL
 AND percentage_laid_off IS NULL;
 
+DELETE 
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT *
+FROM layoffs_staging2;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
+
+
 
 
 
